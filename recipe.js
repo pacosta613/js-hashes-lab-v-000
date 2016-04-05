aed8cb6d9b56cfe14f5024e6e4ff6d69ebcfd7ba
pacosta613 <pedro.acosta613@gmail.com>
2016-04-05 12:59:17 -0400

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function addIngredient(recipe, ingredient, amounts) {
+  recipe[ingredient] = amounts
+  return recipe
+}
+
+function removeIngredient(recipe, ingredient){
+  delete recipe[ingredient];
+  return recipe
+}
+
+function updateIngredient(recipe, ingredient, amounts){
+  recipe[ingredient] = amounts;
+  return recipe
+}
+
+function readRecipe(recipe){
+  for (var key in recipe) {
+    console.log("this recipe calls for " + recipe[key] + " of " + key);
+  }
+}
